e0c79cea4e967e2de3aed685793f8cb98ece9921
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2024-05-29 19:43:30 +0700

    ubah route homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/FrontendDevReactjs-Moh.-Iqbal-Fatoni" element={<HomePage />} />
         <Route path="detail-restaurant/:id" element={<DetailRestaurant />} />
       </Routes>
     </BrowserRouter>
